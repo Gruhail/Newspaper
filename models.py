c346847dba9104db997bb395f28b8aa225c81947
Gruhail <153614012+Gruhail@users.noreply.github.com>
2024-03-12 18:42:54 +0300

    Add files via upload
    Signed-off-by: Gruhail <153614012+Gruhail@users.noreply.github.com>

--- models.py
+++ models.py
@@ -1,51 +1,47 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import Sum
-
-
-NEWS = 'NW'
-ARTICLE = 'AR'
-CATEGORY_CHOISES = (
-    (NEWS, 'News'),
-    (ARTICLE, 'Article'),
-)
-
+from django.urls import reverse_lazy
 
 class Author(models.Model):
     authorUser = models.OneToOneField(User, on_delete=models.CASCADE)
     ratingAuthor = models.SmallIntegerField(default=0)
 
     def update_rating(self):
-        postR = self.post_set.all().aggregate(postRating=Sum('rating'))
-        p_R = 0
-        p_R += postR.get('postRating')
+        postRat = self.post_set.aggregate(postRating=Sum('rating'))
+        pRat = 0
+        pRat += postRat.get('postRating')
 
-        commentR = self.authorUser.comment_set.all().aggregate(commentRating=Sum('rating'))
-        c_R = 0
-        c_R += commentR.get('commentRating')
+        commentRat = self.authorUser.comment_set.aggregate(commentRating=Sum('rating'))
+        cRat = 0
+        cRat += commentRat.get('commentRating')
 
-        self.ratingAuthor = p_R * 3 + c_R
+        self.ratingAuthor = pRat *3 + cRat
         self.save()
-
-    def __str__(self):
-        return f"{self.authorUser}"
 
 
 class Category(models.Model):
     name = models.CharField(max_length=64, unique=True)
 
     def __str__(self):
-        return f"{self.name}"
+        return self.name
 
 
 class Post(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    categoryType = models.CharField(max_length=20, choices=CATEGORY_CHOISES, default=ARTICLE)
-    dataCreations = models.DateTimeField(auto_now_add=True)
+
+    NEWS ='NW'
+    ARTICLE ='AR'
+    CATEGORY_CHOICES = ((NEWS, 'Новость'), (ARTICLE, 'Статья'))
+    categoryType = models.CharField(max_length=2, choices=CATEGORY_CHOICES, default=ARTICLE)
+    dateCreation = models.DateTimeField(auto_now_add=True)
     postCategory = models.ManyToManyField(Category, through='PostCategory')
     title = models.CharField(max_length=128)
     text = models.TextField()
     rating = models.SmallIntegerField(default=0)
+
+    def get_absolute_url(self):
+        return reverse_lazy('post', kwargs={'pk': self.pk})
 
     def like(self):
         self.rating += 1
@@ -56,26 +52,25 @@ class Post(models.Model):
         self.save()
 
     def preview(self):
-        return self.text[0:128] + '...'
+        return self.text[0:123] + '...'
 
     def __str__(self):
-        dataf = 'Post from {}'.format(self.dataCreations.strftime('%d.%m.%Y %H:%M'))
-        return f"{dataf},{self.author},{self.title}"
+        return '%s : %s' % (self.categoryType, self.title)
+
+    class Meta:
+        ordering = ['-dateCreation']
 
 
 class PostCategory(models.Model):
-    postThrough = models.ForeignKey(Post, on_delete=models.CASCADE)
-    categoryThrough = models.ForeignKey(Category, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.postThrough},from the category:  {self.categoryThrough}"
+    postTrough = models.ForeignKey(Post, on_delete=models.CASCADE)
+    categoryTrough = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
 class Comment(models.Model):
     commentPost = models.ForeignKey(Post, on_delete=models.CASCADE)
-    userPost = models.ForeignKey(User, on_delete=models.CASCADE)
+    commentUser = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
-    dataCreation = models.DateTimeField(auto_now_add=True)
+    dateCreation = models.DateTimeField(auto_now_add=True)
     rating = models.SmallIntegerField(default=0)
 
     def like(self):
@@ -85,6 +80,3 @@ class Comment(models.Model):
     def dislike(self):
         self.rating -= 1
         self.save()
-
-    def __str__(self):
-        return f"{self.dataCreation}, {self.userPost}"
--- views.py
+++ views.py
@@ -1,25 +1,68 @@
-from django.http import HttpResponseNotFound
 from django.shortcuts import render
-from django.views.generic import ListView
-from  django.views.generic.base import View
-from .models import *
+from .models import Post, Category
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
+from .forms import NewsForm
+from django.urls import reverse_lazy
+from .filters import PostFilter
 
 
-class AuthorsPage(ListView):
-    model = Author
-    context_object_name = "Authors"
-    template_name = 'news/authors.html'
+class Posts(ListView):
+    model = Post
+    template_name = 'news/base.html'
+    context_object_name = 'news'
+    paginate_by = 10
 
-class PostDetail(View):
-    def get(self, request, pk):
-        ps = Post.objects.get(id=pk)
-        return render(request, "news/posts.html", {'ps':ps})
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
 
-def news_page_list(request):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
 
-    newslist = Post.objects.all().order_by('-rating')[:6]
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['category'] = Category.objects.all()
+    #     return context
 
-    return render(request, 'news/news.html', {'newslist': newslist})
 
-def pageNotFound(request, exception):
-    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'news/post.html'
+    context_object_name = 'post'
+
+
+class PostCreate(CreateView):
+    form_class = NewsForm
+    template_name = 'news/post_create.html'
+    #success_url = reverse_lazy('home')
+
+
+class PostEdit(UpdateView):
+    form_class = NewsForm
+    model = Post
+    # success_url = reverse_lazy('home')
+    template_name = 'news/post_create.html'
+
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name = 'news/deletepost.html'
+    success_url = reverse_lazy('home')
+    context_object_name = 'deletenews'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
